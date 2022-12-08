1671641d51745085b41aa121cb8653f43e2cfa27
VarunDhar <85066986+VarunDhar@users.noreply.github.com>
2022-12-08 17:10:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,23 +12,6 @@ app.listen(3000,function(){
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
-    // let url = "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=2c178e5a3c0684cea8716919b91edc0d&units=metric";
-
-    // https.get(url,function(response){
-    //     response.on("data",function(data){
-    //         const weatherData=JSON.parse(data);
-    //         let temp = weatherData.main.temp;
-    //         let city = weatherData.name;
-    //         let feelslike = weatherData.weather[0].description;
-    //         const imgtag = weatherData.weather[0].icon;
-    //         const imgurl = "http://openweathermap.org/img/wn/"+ imgtag +"@2x.png";
-
-    //         res.set('Content-Type', 'text/html');
-    //         res.write("<h1><center>Temperature in <b>"+city+"</b> is: <b>"+temp+"</b> degrees Celsius and is <b>"+feelslike+"</b></center></h1>");
-    //         res.write("<br><center><img src="+imgurl+"></center>");
-    //         res.send();
-    //     });
-    // });
     
 });
 
